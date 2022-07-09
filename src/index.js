2189dd1dbe3a30f5d6fcad92dc1e9f85eac157c0
Erwin Vásquez <contactwitherwin@gmail.com>
2022-07-08 22:33:05 -0600

    Handlers Module
    Handlers Module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const Express = require("express");
+const{initializeDB}=require("./lib/db");
+const API = Express();
+
+
+
+
+API.listen(3000,() =>{
+    console.log("API IS RUNNING");
+    initializeDB().then(()=> {
+        console.log("READYYYY")
+    });
+});
